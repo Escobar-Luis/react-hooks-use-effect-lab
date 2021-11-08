9bb519df6526682b70f45cffcd2d3cfc4e24a1e4
Luis Escobar <lae311@stern.nyu.edu>
2021-11-08 11:20:13 -0500

    commit tree b6e41b709ecb389f6bdaeb9ae91b98ed4b8369f5

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  // add useEffect code, will be invoke after my dom is rendered
+  useEffect(() => {
+    //first check if the timer is at zero se we can reset it back to 10 and set onAnswered to false so our app either goes to the next question or returns nothing if all questions have been answered
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1)
+  }, 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [timeRemaining])
+  //Remember our dependancies have to be in brackets
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
+  // deconstructing the question to access its individual keys
   const { id, prompt, answers, correctIndex } = question;
 
   return (
